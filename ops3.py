277e4e8d19b6cf29c21276f1ae546835a5b3507d
MACHINE <benjaminstephan1997@gmail.com>
2025-01-07 19:07:24 -0600

    ops3.py

---
+++ ops3.py
@@ -0,0 +1,12 @@
+john =  3
+mary = 5
+adam = 6
+
+print(john, mary, adam, sep=', ', end='\n')
+total_apples = john + mary + adam
+print("Total apples: ", total_apples)
+
+# + - * / //
+
+print(10 / 8)
+print(10 // 8 + 9 + 9 - 10 / 8 - 3)
